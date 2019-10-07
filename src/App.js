6b7b108556fd46e5dbe5e6e7c3dd8eab78d8c660
Samuel Fortson <sfortson@uber.com>
2019-10-06 20:59:31 -0400

    Add Paypal Button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PaypalButton from './PaypalButton'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+const CLIENT = {
+  sandbox: process.env.REACT_APP_PAYPAL_CLIENT_ID_SANDBOX,
+  production: process.env.REACT_APP_PAYPAL_CLIENT_ID_PRODUCTION
 }
 
-export default App;
+const ENV = process.env.NODE_ENV === 'production' ? 'production' : 'sandbox'
+
+class App extends Component<> {
+  render() {
+    const onSuccess = payment => console.log('Successful payment!', payment)
+    const onError = error =>
+      console.log('Erroneous payment OR failed to load script!', error)
+    const onCancel = data => console.log('Cancelled payment!', data)
+    return (
+      <Container>
+        <Row>
+          <Col
+            lg={9}
+            className="justify-content-lg-center"
+            style={{ paddingTop: '72px' }}>
+            <div>
+              <PaypalButton
+                client={CLIENT}
+                env={ENV}
+                commit={true}
+                currency={'USD'}
+                total={150}
+                onSuccess={onSuccess}
+                onError={onError}
+                onCancel={onCancel}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default App
